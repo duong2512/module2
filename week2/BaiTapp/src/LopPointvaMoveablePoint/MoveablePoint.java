a0bb4ff2e49e5fe705efe51e2c51db863f0e5610
duong2512 <tungduong25122002@gmail.com>
2022-05-22 12:34:37 +0700

    ok

---
+++ week2/BaiTapp/src/LopPointvaMoveablePoint/MoveablePoint.java
@@ -0,0 +1,8 @@
+package LopPointvaMoveablePoint;
+
+public class MoveablePoint extends Point{
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+
+}
---
+++ week2/BaiTapp/src/LopPointvaMoveablePoint/Point.java
@@ -0,0 +1,50 @@
+package LopPointvaMoveablePoint;
+
+public class Point {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point() {
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float [] getXY() {
+        float []XY = new float[]{
+                this.x,this.y
+        };
+        return XY;
+    }
+
+    public void setXY(){
+        this.x= x;
+        this.y= y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
