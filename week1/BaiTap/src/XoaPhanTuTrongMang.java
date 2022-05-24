d3d67387d6a64a9291852a26a7911374c5057a59
duong2512 <tungduong25122002@gmail.com>
2022-05-24 23:03:28 +0700

    ok

--- week1/BaiTap/src/XoaPhanTuTrongMang.java
+++ week1/BaiTap/src/XoaPhanTuTrongMang.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class XoaPhanTuTrongMang {
     static int[] xoaPhanTu(int[] arr, int value, int c) {
         for (int i = 0; i < arr.length; i++) {
@@ -8,9 +10,27 @@ public class XoaPhanTuTrongMang {
                 } arr[arr.length-1]=0;
                 return xoaPhanTu(arr,value,c);
             }
-            int []arr1 = new int[arr.length-c];
-            System.arraycopy(arr,0,arr1,0,arr1.length);
-            return arr1;
+        } int []arr1 = new int[arr.length-c];
+        System.arraycopy(arr,0,arr1,0,arr1.length);
+        return arr1;
+    }
+
+    public static void main(String[] args) {
+        int[]arr = {10,4,6,7,8,6,0,0,0,0};
+        System.out.println("Mảng ban đầu là :");
+        for (int a:arr
+             ) {
+            System.out.println(a);
+        }
+
+        System.out.println("Nhập phần tử muốn xóa trong mảng: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        int c = 0;
+        System.out.println("Mảng sau khi xóa: ");
+        for (int a:xoaPhanTu(arr,value,c)
+             ) {
+            System.out.println(a);
         }
     }
 
--- week3/ThucHanhh/src/TrienKhaiStackSuDungLopLinkedList/GenericStackClient.java
+++
@@ -1,4 +0,0 @@
-package TrienKhaiStackSuDungLopLinkedList;
-
-public class GenericStackClient {
-}
