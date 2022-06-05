2e740a68b9a22dbcad70271ccaba106216e3792e
duong2512 <tungduong25122002@gmail.com>
2022-06-05 17:36:17 +0700

    ok

--- week2/BaiTapp/src/LopPoint2DvaPoint3D/Main.java
+++ week2/BaiTapp/src/LopPoint2DvaPoint3D/Main.java
@@ -3,6 +3,7 @@ package LopPoint2DvaPoint3D;
 public class Main {
     public static void main(String[] args) {
         Point2D point2D = new Point2D(2,4);
+
         System.out.println(point2D);
     }
 }
---
+++ week2/ThucHanh/src/edibale/Edible.java
@@ -0,0 +1,5 @@
+package edibale;
+
+public interface Edible {
+    String howToEat();
+}
