8b53df833c921f192fd164e9b9ae55ef37e7b671
duong2512 <tungduong25122002@gmail.com>
2022-06-05 16:59:26 +0700

    ok

---
+++ week2/BaiTapp/src/LopPointvaMoveablePoint/Main.java
@@ -0,0 +1,8 @@
+package LopPointvaMoveablePoint;
+
+public class Main {
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint(2,4);
+        System.out.println(moveablePoint);
+    }
+}
